cd760473819d4078cbb24ef962b07626244a4e04
zhengtaocui <Zhengtao.Cui@rtx.com>
2025-10-07 21:17:27 +0000

    1) Deleted the example data TWDEF/ from the original UEB code becuase this original data has a watershed that has a grid of cells while this BMI version only processes one cell, 2) Added the output file Point00.txt.original_parallel_out which is the output from the original UEB code. This file can be compared with the model output or benchmark file to verify the code. 3) Added a benchmark file Point00.txt.bench_mark for verification of the test run.

--- examples/TWDEF/runTWDEF.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/opt/zlib-1.2.13/lib:/opt/curl-7.88.1/lib:/opt/parallel_hdf5-1.12.3/lib:/opt/netcdf-c-4.8.1/lib:/opt/pnetcdf-1.12.3/lib:$LD_LIBRARY_PATH
-#export CPPFLAGS=-I/path to /pnclibs/include
-#export LDFLAGS=-L/path to /pnclibs/lib 
-###reuse GCC-4.8
-###cd $PBS_O_WORKDIR
-###echo "Working directory: $PBS_O_WORKDIR"
-mpirun -np 2 ../uebpar control_dist.dat
